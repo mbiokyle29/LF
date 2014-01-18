6a9587645f210a37dadb70a4426be8d65c0dc35b
Kyle <mbio.kyle@gmail.com>
2014-01-18 17:20:11 -0600

    Reworked matching, made more intelligent changed Match id to potential

--- Found.pl
+++ Found.pl
@@ -34,6 +34,8 @@ get '/ios-7/end/found' => sub { shift->render(json => \@all_losts ) };
 
 sub match_maker
 {
+  my @unimp = qw(the of to and a in is it you that he was for on are with as I his they be at one have and this);
+
   my $mongo_client = MongoDB::MongoClient->new
   (
     host => 'linus.mongohq.com',
@@ -46,33 +48,62 @@ sub match_maker
   my $db = $mongo_client->get_database('LF');
   my $losts = $db->get_collection('Lost');
   my $founds = $db->get_collection('Found');
+
   my $lost_c = $losts->query( {Matched => 0} );
 
   while(my $lost = $lost_c->next)
   {
     my $lost_ref = MongoDB::DBRef->new( db => 'LF', ref => $losts, id => $lost->{_id} );
     my $best_ref;
-    my $most_matches = 0;
+    my $best_total = 0;
+
+    # Lost Vars for matching
+    my $l_loc = $lost->{Location};
+    my $l_desc = $lost->{Description};
+    my $l_item = $lost->{Item};
+    my @l_tags = @{ $lost->{Tags}};
 
     my $found_c = $founds->query({Matched => 0});
+
     while(my $found = $found_c->next)
     {
-      my $matches;
       my $found_ref = MongoDB::DBRef->new( db => 'LF', ref => $founds, id => $found->{_id} );
-      if(amatch($lost->{Location},["i"],$found->{Location}))
+
+      # Found Vars for matching
+      my $f_loc = $found->{Location};
+      my $f_desc = $found->{Description};
+      my $f_item = $found->{Item};
+      my @f_tags = @{ $found->{Tags}};
+
+      my $total = 0;
+
+      if(amatch($l_loc,["i"],$f_loc))
       {
-        foreach my $tag (@{$lost->{'Tags'}})
+        say "Locations matched";
+        say "Lets check tags";
+        foreach my $tagl (@l_tags)
         {
-          my $count = amatch($tag,["i"], @{$found->{Tags}});
-          $matches+=$count;
+          $tagl =~ s/\s//g;
+          next if( (length($tagl) < 2) || grep(/^$tagl$/, @unimp) );
+          foreach my $tagf (@f_tags)
+          {
+            $tagf =~ s/^\s//g;
+            next if( (length($tagf) < 2) || grep(/^$tagf$/, @unimp) );
+
+            say "Checking lost tag $tagl against $tagf";
+            if(lc($tagl) eq lc($tagf))    { $total++;   next; }
+            if(amatch($tagl,["i"],$tagf)) { $total+=.9; next; }
+            if(index($tagl, $tagf) != -1) { $total+=.2; next; }
+            if(index($tagf, $tagl) != -1) { $total+=.2; next; }
+          }
         }
+        if($total > $best_total) { $best_total = $total; $best_ref = $found_ref; }
       }
-      if($matches && $matches > $most_matches) {  $most_matches = $matches; $best_ref = $found_ref; }
     }
-    if($best_ref)
+    if($best_ref && $best_total > 1)
     {
-      $losts->update({ _id => $lost_ref->id }, { '$set' => { Matched => 1, Match_id => $best_ref->id } }, { 'upsert' => 1 } );
-      $founds->update({ _id => $best_ref->id }, { '$set' => { Matched => 1, Match_id => $lost_ref->id } }, { 'upsert' => 1 } );
+      $losts->update({ _id => $lost_ref->id }, { '$set' => { Matched => 1, PMatch_id => $best_ref->id } }, { 'upsert' => 1 } );
+      $founds->update({ _id => $best_ref->id }, { '$set' => { Matched => 1, PMatch_id => $lost_ref->id } }, { 'upsert' => 1 } );
     }
   }
 }
--- matchMaker.pl
+++ matchMaker.pl
@@ -6,6 +6,8 @@ use MongoDB::OID;
 use String::Approx 'amatch';
 use feature 'say';
 
+my @unimp = qw(the of to and a in is it you that he was for on are with as I his they be at one have and this);
+
 my $mongo_client = MongoDB::MongoClient->new
 (
   host => 'linus.mongohq.com',
@@ -22,36 +24,58 @@ my $founds = $db->get_collection('Found');
 
 my $lost_c = $losts->query( {Matched => 0} );
 
-
+## TODO Implement skipping
 while(my $lost = $lost_c->next)
 {
   my $lost_ref = MongoDB::DBRef->new( db => 'LF', ref => $losts, id => $lost->{_id} );
   my $best_ref;
-  my $most_matches = 0;
+  my $best_total = 0;
+
+  # Lost Vars for matching
+  my $l_loc = $lost->{Location};
+  my $l_desc = $lost->{Description};
+  my $l_item = $lost->{Item};
+  my @l_tags = @{ $lost->{Tags}};
 
   my $found_c = $founds->query({Matched => 0});
+
   while(my $found = $found_c->next)
   {
-    my $matches;
     my $found_ref = MongoDB::DBRef->new( db => 'LF', ref => $founds, id => $found->{_id} );
-    if(amatch($lost->{Location},["i"],$found->{Location}))
-    {
-      foreach my $tag (@{$lost->{'Tags'}})
-      {
-        my $count = amatch($tag,["i"], @{$found->{Tags}});
-        $matches+=$count;
-      }
-    }
-    if($matches && $matches > $most_matches)
-    {
-      $most_matches = $matches;
-      $best_ref = $found_ref;
-    }
-  }
 
-  if($best_ref)
-  {
-    $losts->update({ _id => $lost_ref->id }, { '$set' => { Matched => 1, Match_id => $best_ref->id } }, { 'upsert' => 1 } );
-    $founds->update({ _id => $best_ref->id }, { '$set' => { Matched => 1, Match_id => $lost_ref->id } }, { 'upsert' => 1 } );
+    # Found Vars for matching
+    my $f_loc = $found->{Location};
+    my $f_desc = $found->{Description};
+    my $f_item = $found->{Item};
+    my @f_tags = @{ $found->{Tags}};
+
+    my $total = 0;
+
+    if(amatch($l_loc,["i"],$f_loc))
+    {
+      say "Locations matched";
+      say "Lets check tags";
+      foreach my $tagl (@l_tags)
+      {
+        $tagl =~ s/\s//g;
+        next if( (length($tagl) < 2) || grep(/^$tagl$/, @unimp) );
+        foreach my $tagf (@f_tags)
+        {
+          $tagf =~ s/^\s//g;
+          next if( (length($tagf) < 2) || grep(/^$tagf$/, @unimp) );
+          say "Checking lost tag $tagl against $tagf";
+          if(amatch($tagl,["i"],$tagf))
+          {
+            say "l tag $tagl is a match for r tag $tagf";
+            $total+=.9;
+            if(lc($tagl) eq lc($tagf))    { $total++; }
+            if(index($tagl, $tagf) != -1) { $total+=.2; }
+            if(index($tagf, $tagl) != -1) { $total+=.2; }
+          }
+        }
+      }
+      ## matches has raw tag match, add some stuff ##
+      if($total > $best_total) { $best_total = $total; $best_ref = $found_ref; }
+    }
   }
 }
